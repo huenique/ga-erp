7f30ec8bee12eab0479e7b06693d0cbf569aec76
Hju Kneyck Flores <hjucode@gmail.com>
2022-01-04 15:28:24 +0800

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./static/App.css";
+
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/icons.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable strict */
+/* globals Chart:false, feather:false */
+
+(function () {
+  "use strict";
+
+  feather.replace({ "aria-hidden": "true" });
+})();
